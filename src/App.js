97b3f885f09c9af7bcbb7c44e6543cf8a772aceb
Prasanta kumar <pksitha14@gmail.com>
2024-07-21 08:33:19 +0530

    Route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
-import "./App.css";
+import * as React from "react";
+import { createBrowserRouter, Outlet } from "react-router-dom";
 import Header from "./component/header/Header";
 import Body from "./component/body/Body";
+import About from "./component/about/About";
+import Contact from "./component/contact/Contact";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Body />
+const Layout = () => (
+  <div>
+    <Header />
+    <div style={{ margin: "0px 20px" }}>
+      <Outlet />
     </div>
-  );
-}
+  </div>
+);
 
-export default App;
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+export default router;
